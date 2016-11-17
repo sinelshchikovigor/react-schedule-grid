bc97dae251d329d3eeac10cfb5f8a64de56208c1
Igor Sinelshchikov <TOXICSIS@gmail.com>
2016-11-17 15:41:52 +0300

    Example app added (#7)
    * Example app added

---
+++ example/example.ts
@@ -0,0 +1,11 @@
+import { subjects, entities } from './mock.data';
+import { SchedulerWrapper } from '../src/scheduler.wrapper';
+
+const editorContent = {
+    subjects,
+    entities
+};
+
+SchedulerWrapper.initialize('app');
+SchedulerWrapper.setDateRange(new Date(2016, 9, 1), new Date(2016, 9, 15));
+SchedulerWrapper.loadData(editorContent);
